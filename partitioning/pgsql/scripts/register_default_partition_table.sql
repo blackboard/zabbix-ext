8b9a0105259c32014bcc9e2450014ba84acff7c5
nizhi <ni.zhi@blackboard.com>
2013-11-14 21:52:01 -0500

    Change the default reserve months from 12 to 1 for history and history_uint tables

--- partitioning/pgsql/scripts/register_default_partition_table.sql
+++ partitioning/pgsql/scripts/register_default_partition_table.sql
@@ -1,5 +1,5 @@
 SELECT zabbix_maintaince.register_partition_table( 'EVENTS', 'CLOCK', 12, 3 );
-SELECT zabbix_maintaince.register_partition_table( 'HISTORY_UINT', 'CLOCK', 12, 3 );
-SELECT zabbix_maintaince.register_partition_table( 'HISTORY', 'CLOCK', 12, 3 );
+SELECT zabbix_maintaince.register_partition_table( 'HISTORY_UINT', 'CLOCK', 1, 3 );
+SELECT zabbix_maintaince.register_partition_table( 'HISTORY', 'CLOCK', 1, 3 );
 SELECT zabbix_maintaince.register_partition_table( 'TRENDS_UINT', 'CLOCK', 12, 3 );
 SELECT zabbix_maintaince.register_partition_table( 'TRENDS', 'CLOCK', 12, 3 );
