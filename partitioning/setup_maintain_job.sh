0a42ca3df2e2c9bc30a3d5b6e628258a33715d9a
Guoyuli <li.guoyu@blackboard.com.cn>
2013-11-21 17:24:59 +0800

    add cron job script

---
+++ partitioning/setup_maintain_job.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+BASE_DIR=$(dirname $0)
+if [ $BASE_DIR = '.' ] 
+then
+  BASE_DIR=$PWD
+fi
+
+tmp=${TMPDIR:-/tmp}/xyz.$$
+maintain_job=${BASE_DIR}/maintain.sh
+trap "rm -f $tmp; exit 1" 0 1 2 3 13 15
+crontab -l | sed '%${maintain_job}%d' > $tmp  # Capture crontab; delete old entry
+echo "0 1 * * sun ${maintain_job}" >> $tmp
+crontab < $tmp
+rm -f $tmp
+trap 0
