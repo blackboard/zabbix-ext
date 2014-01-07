46fbd6c7a5c9d28f6f65b123235285a56187f399
Li Guoyu <li.guoyu@blackboard.com>
2014-01-07 05:11:41 -0500

    More functions and tests

---
+++ tools/rpcapi/src/main/scala/blackboard/monitoring/zabbix/AutoRegistrationAction.scala
@@ -0,0 +1,10 @@
+package blackboard.monitoring.zabbix
+
+/**
+ * Need hostname, metadata, hostip
+ */
+object AutoRegistrationAction extends ZabbixRpcApi {
+  def addHost() = {}
+  def applyTemplates() = {}
+  def assignGroups() = {}
+}
---
+++ tools/rpcapi/src/main/scala/blackboard/monitoring/zabbix/ZabbixCommonOps.scala
@@ -0,0 +1,26 @@
+package blackboard.monitoring.zabbix
+
+import play.api.libs.json._
+
+trait ZabbixCommonOps extends ZabbixRpcApi {
+  def getGroupIdByName(name: String) = {
+    val result = hostgroup.get(Json.obj(
+      "filter" -> Json.obj(
+        "name" -> Json.arr(name))))
+    extractUniqueId("groupid", result)
+  }
+
+  def getHostIdByName(hostname: String) = {
+    val result = host.get(Json.obj(
+      "filter" -> Json.obj(
+        "host" -> Json.arr(hostname))))
+    extractUniqueId("hostid", result)
+  }
+
+  def getProxyIdByName(proxyName: String) = {
+    val result = proxy.get(Json.obj(
+      "filter" -> Json.obj(
+        "host" -> Json.arr(proxyName))))
+    extractUniqueId("proxyid", result)
+  }
+}
